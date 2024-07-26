07ca07dadb59b6e0b42615016180e73eb64cac42
dwm <dwm@mcplex.net>
2024-07-26 16:25:54 -0400

    Added comments

--- classes/include/DwmMcweatherConfig.hh
+++ classes/include/DwmMcweatherConfig.hh
@@ -1,7 +1,7 @@
 //===========================================================================
 // @(#) $DwmPath$
 //===========================================================================
-//  Copyright (c) Daniel W. McRobb 2020, 2022
+//  Copyright (c) Daniel W. McRobb 2020, 2022, 2024
 //  All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
@@ -42,7 +42,6 @@
 #ifndef _DWMMCWEATHERCONFIG_HH_
 #define _DWMMCWEATHERCONFIG_HH_
 
-
 #include <string>
 
 #include "DwmMcweatherServiceConfig.hh"
@@ -59,87 +58,94 @@ namespace Dwm {
     {
     public:
       //----------------------------------------------------------------------
-      //!  
+      //!  Default constructor
       //----------------------------------------------------------------------
       Config();
       
       //----------------------------------------------------------------------
-      //!  
+      //!  Parse the configuration from the file at the given @c path.
+      //!  Returns true on success, false on failure.
       //----------------------------------------------------------------------
       bool Parse(const std::string & path);
 
       //----------------------------------------------------------------------
-      //!  
+      //!  Returns the path to the directory where weather conditions will
+      //!  be cached.
       //----------------------------------------------------------------------
       const std::string & CacheDirectory() const;
 
       //----------------------------------------------------------------------
-      //!  
+      //!  Sets and returns the path to the directory where weather
+      //!  conditions will be cached.
       //----------------------------------------------------------------------
       const std::string & CacheDirectory(const std::string & cacheDir);
 
       //----------------------------------------------------------------------
-      //!  
+      //!  Returns the syslog facility that will be used for logging.
       //----------------------------------------------------------------------
       const std::string & SyslogFacility() const;
       
       //----------------------------------------------------------------------
-      //!  
+      //!  Sets and returns the syslog facility that will be used for
+      //!  logging.
       //----------------------------------------------------------------------
       const std::string & SyslogFacility(const std::string & facility);
 
       //----------------------------------------------------------------------
-      //!  
+      //!  Returns the minimum level of syslog message that will be logged.
       //----------------------------------------------------------------------
       const std::string & SyslogLevel() const;
 
       //----------------------------------------------------------------------
-      //!  
+      //!  Sets and returns minimum the level of syslog message that will be
+      //!  logged.
       //----------------------------------------------------------------------
       const std::string & SyslogLevel(const std::string & level);
 
       //----------------------------------------------------------------------
-      //!  
+      //!  Returns true if we will log file name and line number in syslog
+      //!  messages.
       //----------------------------------------------------------------------
       bool SyslogLocations() const;
 
       //----------------------------------------------------------------------
-      //!  
+      //!  if @c logLocations is @c true, we will log file name and line
+      //!  number in syslog messages.
       //----------------------------------------------------------------------
       bool SyslogLocations(bool logLocations);
 
       //----------------------------------------------------------------------
-      //!  
+      //!  Returns true if mcweatherd should run a server.
       //----------------------------------------------------------------------
       bool RunService() const;
       
       //----------------------------------------------------------------------
-      //!  
+      //!  If @c runService is true, mcweatherd should run a server.
       //----------------------------------------------------------------------
       bool RunService(bool runService);
 
       //----------------------------------------------------------------------
-      //!  
+      //!  Returns the network service configuration.
       //----------------------------------------------------------------------
       const ServiceConfig & Service() const;
 
       //----------------------------------------------------------------------
-      //!  
+      //!  Sets and returns the network service configuration.
       //----------------------------------------------------------------------
       const ServiceConfig Service(const ServiceConfig & service);
       
       //----------------------------------------------------------------------
-      //!  
+      //!  Returns the weather configuration.
       //----------------------------------------------------------------------
       const WeatherConfig & Weather() const;
       
       //----------------------------------------------------------------------
-      //!  
+      //!  Sets and returns the weather configuration.
       //----------------------------------------------------------------------
       const WeatherConfig & Weather(const WeatherConfig & weather);
 
       //----------------------------------------------------------------------
-      //!  
+      //!  ostream output operator
       //----------------------------------------------------------------------
       friend std::ostream &
       operator << (std::ostream & os, const Config & cfg);
